95b734c3db78020f35e99ec61526bd58d7243cf6
Shingo Tomishima <shingo.tomiy@gmail.com>
2023-11-10 21:07:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import streamlit as st
+st.title("Data Exploration App")
+  
+# Data loading and display
+df = load_data("https://ai-jobs.net/salaries/download/salaries.csv")
+st.write("Data Overview:", df)
+  
+st.write(df.describe())
 
-st.write('hello')
+  # Linear Regression
+x_col, y_col = st.selectbox('Select X Column', df.columns), st.selectbox('Select Y Column', df.columns)
+X = df[[x_col]].values.reshape(-1, 1)
+Y = df[y_col].values
+model = LinearRegression()
+model.fit(X, Y)
+st.write(f"Coefficient: {model.coef_[0]}, Intercept: {model.intercept_}")
 
